cae34442e9112953766cff0ae55e05600be32917
鲜文坤 <845982991@qq.com>
2025-08-10 01:02:22 +0800

    Fix path from LeranBlog to LearnBlog

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LeranBlog/', // GitHub Pages 部署时需要这个路径
+  base: '/LearnBlog/', // GitHub Pages 部署时需要这个路径
   build: {
     outDir: 'dist', 
     sourcemap: false
